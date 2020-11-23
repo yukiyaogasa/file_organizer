346025b687271c4ad8d71deb1ae162430ccf54a4
y-o-anicca <yodog712@gmail.com>
2020-11-23 17:28:58 +0900

    手続き処理のクラス化

--- lib/file_organizer.rb
+++ lib/file_organizer.rb
@@ -2,66 +2,118 @@ require 'fssm'
 require 'pry'
 require 'fileutils'
 
-BASE_DIR = "/Users/#{`whoami`.chomp}"
-FOLDER_TO_TRACK = "#{BASE_DIR}/downloads/"
-FILE_TYPE_ENUM = {
-  document: ['text', 'txt', 'pdf']
-}
+class FileOrganizer
+  attr_accessor :file_name, :tracking_folder, :old_dest, :organizable
 
-def create_action(base, file)
-  open('log.txt', 'a'){|f|
-    f.puts base + "/"  + file + " was created at " + `date`
-  }
-  old_dest = FOLDER_TO_TRACK + file
-  ext = File.extname(file).sub('.', '')
-  case ext
-  when *FILE_TYPE_ENUM[:document]
-    p "Detect document..."
-    FileUtils.mv(FOLDER_TO_TRACK + file, new_dest(file, 'document'))
-  else
-    p 'Detect other...'
+  def self.organize(file_name, tracking_folder)
+    self.new(file_name, tracking_folder) do |organizer|
+      FileUtils.mv(organizer.old_dest, organizer.organizable.new_dest)
+    end
+  end
+
+  private
+
+  def initialize(file_name, tracking_folder)
+    @file_name = file_name
+    @tracking_folder = tracking_folder
+    @old_dest = tracking_folder + file_name
+    @organizable = Organizable.new(file_name)
+
+    yield self if block_given?
   end
 end
 
-def update_action(base, file)
-  open('log.txt', 'a'){|f|
-    f.puts base + "/"  + file + " was updated at " + `date`
-  }
-end
+class Organizable
+  attr_accessor :file_name
 
-def delete_action(base, file)
-  open('log.txt', 'a'){|f|
-    f.puts base + "/" + file + " was deleted at " + `date`
+  BASE_DIR = "/Users/#{`whoami`.chomp}"
+  FILE_TYPE_ENUM = {
+    document: ['text', 'txt', 'pdf']
   }
-end
 
-def new_dest(file_name, file_type)
-  folder_name = case file_type
-  when 'document'
-    'Documents'
-  when 'picture'
-    'Pictures'
-  when 'csv'
-    'Csvs'
-  else
-    'Others'
+  def initialize(file_name)
+    @file_name = file_name
   end
-  "#{BASE_DIR}/#{folder_name}/#{file_name}"
-end
+
+  def new_dest
+    "#{BASE_DIR}/#{folder_name}/#{file_name}"
+  end
   
+  private
 
+  def extention
+    @extention ||= File.extname(file_name).sub('.', '')
+  end
 
-p "監視対象 #{FOLDER_TO_TRACK}"
-FSSM.monitor(FOLDER_TO_TRACK,'**/*') do
- 
-  create do |base,file|
-      create_action(base, file)
+  def file_type
+    case extention
+    when *FILE_TYPE_ENUM[:document]
+      p "Detect document..."
+      'document'
+    else
+      p 'Detect other...'
+      'other'
+    end
   end
-  update do |base,file|
-      update_action(base, file)
+
+  def folder_name
+    case file_type
+    when 'document'
+      'Documents'
+    when 'picture'
+      'Pictures'
+    when 'csv'
+      'Csvs'
+    else
+      'Others'
+    end
   end
-   delete do |base,file|
-      delete_action(base, file)
+end
+
+class FileObserver
+  BASE_DIR = "/Users/#{`whoami`.chomp}"
+  FOLDER_TO_TRACK = "#{BASE_DIR}/downloads/"
+
+  def initialize
+    p "監視対象 #{FOLDER_TO_TRACK}"
+    FSSM.monitor(FOLDER_TO_TRACK,'**/*') do
+      create do |base,file|
+        open('log.txt', 'a'){|f|
+          f.puts base + "/"  + file + " was created at " + `date`
+        }
+        FileOrganizer.organize(file, FOLDER_TO_TRACK)
+      end
+      # update do |base,file|
+      #   update_action(base, file)
+      # end
+      # delete do |base,file|
+      #   delete_action(base, file)
+      # end
+    end
   end
- 
- end
+
+  def aa
+    p 'aa'
+  end
+
+  def create_action(base, file)
+    open('log.txt', 'a'){|f|
+      f.puts base + "/"  + file + " was created at " + `date`
+    }
+    FileOrganizer.organize(file, FOLDER_TO_TRACK)
+  end
+  
+  # def update_action(base, file)
+  #   open('log.txt', 'a'){|f|
+  #     f.puts base + "/"  + file + " was updated at " + `date`
+  #   }
+  # end
+  
+  # def delete_action(base, file)
+  #   open('log.txt', 'a'){|f|
+  #     f.puts base + "/" + file + " was deleted at " + `date`
+  #   }
+  # end
+end
+
+FileObserver.new
