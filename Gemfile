3ede2909d423f1015314ca6c27960824d07d20a3
y-o-anicca <yodog712@gmail.com>
2021-02-06 19:17:21 +0900

    不要なファイル削除

--- Gemfile
+++ Gemfile
@@ -2,6 +2,5 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in file_organizer.gemspec
 
-gem "rake", "~> 12.0"
 gem "rspec", "~> 3.0"
 gem "fssm"
--- spec/file_organizer_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe FileOrganizer do
-  it "has a version number" do
-    expect(FileOrganizer::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
